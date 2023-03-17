5d30985bd8a20e8e0bfea10b3bb0e99fc7ceb52e
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-03-17 20:29:42 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static bool end = false;
+        static void Main(string[] args)
+        {
+
+            Receive();
+            while (!end)
+            {
+                Thread.Sleep(100);
+            }
+        }
+        static async void Receive()
+        {
+            using var udpClient = new UdpClient(8001);
+            var brodcastAddress = IPAddress.Parse("235.5.5.11");
+
+            udpClient.JoinMulticastGroup(brodcastAddress);
+            Console.WriteLine("Начало прослушивания сообщений");
+            while (true)
+            {
+                var result = await udpClient.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                if (message == "END") break;
+                Console.WriteLine(message);
+            }
+
+            udpClient.DropMulticastGroup(brodcastAddress);
+            Console.WriteLine("Udp-клиент завершил свою работу");
+            end = true;
+        }
+    }
+}
---
+++ MetanitUDP/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace MetanitUDP
+{
+    internal class Program
+    {
+        static bool end = false;
+        static void Main(string[] args)
+        {
+
+            Sender();
+            while(!end) 
+            {
+                Thread.Sleep(100);
+            }
+            
+        }
+        static async void Sender()
+        {
+            var messages = new string[] { "Hello World!", "Hello METANIT.COM", "Hello work", "END" };
+            var brodcastAddress = IPAddress.Parse("235.5.5.11");
+            using var udpSender = new UdpClient();
+            Console.WriteLine("Начало отправки сообщений...");
+            // отправляем сообщения
+            foreach (var message in messages)
+            {
+                Console.WriteLine($"Отправляется сообщение: {message}");
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await udpSender.SendAsync(data, new IPEndPoint(brodcastAddress, 8001));
+                await Task.Delay(1000);
+            }
+            end = true;
+        }
+    }
+}
